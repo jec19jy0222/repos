4dc040e6c48c30de75fd7d0b84be5dd1a4d6ff3b
19jy0222 <19jy0222@PC33.jynet2.jec.ac.jp>
2020-11-12 09:45:53 +0900

    mod 乱数を10回から20回に増やして表示する

--- project0/src/project0/HelloWorld.java
+++ project0/src/project0/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int cnt = 0;
-		cnt =(int)( Math.random() * 10 ) + 1;
+		cnt =(int)( Math.random() * 11 ) + 10;
 		for(int i  = 0; i < cnt; i++) {
 			System.out.println("Hello World");
 		}
